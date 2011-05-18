5e19ec6d8828c35cf8fd60dd1cd82023129d0544
Craig Campbell <iamcraigcampbell@gmail.com>
2011-05-17 20:36:38 -0400

    update year to 2011

--- ChromePhp.php
+++ ChromePhp.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2010 Craig Campbell
+ * Copyright 2011 Craig Campbell
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
