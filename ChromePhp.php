81380c72a51af696bda08b21ef3b3c0b22509da2
Craig Campbell <iamcraigcampbell@gmail.com>
2012-02-09 00:42:39 -0500

    Increase version and year

--- ChromePhp.php
+++ ChromePhp.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2011 Craig Campbell
+ * Copyright 2012 Craig Campbell
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
